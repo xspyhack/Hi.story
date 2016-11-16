07b49503f06e31e5e00a4d83ff8c7a09fe177735
xspyhack <xspyhack@gmail.com>
2016-11-16 22:08:25 +0800

    Add Listenable

--- Hi.kit/Protocols/Listenable.swift
+++ Hi.kit/Protocols/Listenable.swift
@@ -7,3 +7,68 @@
 //
 
 import Foundation
+
+public struct Listener<T>: Hashable {
+    
+    let name: String
+    
+    public typealias Action = (T) -> Void
+    let action: Action
+    
+    public var hashValue: Int {
+        return name.hashValue
+    }
+}
+
+public func ==<T>(lhs: Listener<T>, rhs: Listener<T>) -> Bool {
+    return lhs.name == rhs.name
+}
+
+public class Listenable<U> {
+    
+    public typealias SetterAction = (U) -> Void
+    
+    public var value: U {
+        didSet {
+            setterAction(value)
+            
+            for listener in listenerSet {
+                listener.action(value)
+            }
+        }
+    }
+    
+    var setterAction: (U) -> Void
+    
+    var listenerSet: Set<Listener<U>> = []
+    
+    public func bindListener(with name: String, action: @escaping Listener<U>.Action) {
+        let listener = Listener(name: name, action: action)
+        
+        listenerSet.insert(listener)
+    }
+    
+    public func bindAndFireListener(with name: String, action: @escaping Listener<U>.Action) {
+        bindListener(with: name, action: action)
+        
+        action(value)
+    }
+    
+    public func removeListener(with name: String) {
+        for listener in listenerSet {
+            if listener.name == name {
+                listenerSet.remove(listener)
+                break
+            }
+        }
+    }
+    
+    public func removeAllListeners() {
+        listenerSet.removeAll(keepingCapacity: false)
+    }
+    
+    public init(_ v: U, setterAction action: @escaping SetterAction) {
+        value = v
+        setterAction = action
+    }
+}
