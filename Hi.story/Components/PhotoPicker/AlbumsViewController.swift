31bd0e789f466a420cd31b8b14edc6b214612a51
xspyhack <xspyhack@gmail.com>
2016-12-26 20:11:09 +0800

    add PhotoPicker

--- Hi.story/Components/PhotoPicker/AlbumsViewController.swift
+++ Hi.story/Components/PhotoPicker/AlbumsViewController.swift
@@ -7,89 +7,268 @@
 //
 
 import UIKit
+import Photos
+import Hikit
 
-class AlbumsViewController: UITableViewController {
+struct Album {
+    var asset: PHFetchResult<PHAsset>?
+    var count = 0
+    var name: String?
+    var startDate: Date?
+    var identifier: String?
+}
 
+private let defaultAlbumIdentifier = "com.xspyhack.History.photoPicker"
+
+final class AlbumCell: UITableViewCell, Reusable {
+    
+    lazy var countLabel: UILabel = {
+        let lable = UILabel()
+        lable.font = UIFont.systemFont(ofSize: 12.0)
+        return lable
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let lable = UILabel()
+        lable.font = UIFont.systemFont(ofSize: 14.0)
+        return lable
+    }()
+    
+    lazy var posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+   
+    private func setup() {
+        
+        addSubview(posterImageView)
+        addSubview(titleLabel)
+        addSubview(countLabel)
+        
+        posterImageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        countLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let views: [String: Any] = [
+            "posterImageView": posterImageView,
+            "titleLabel": titleLabel,
+            "countLabel": countLabel,
+        ]
+        
+        let h = NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[posterImageView]-16-[titleLabel]-|", options: [], metrics: nil, views: views)
+        let posterImageViewV = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[posterImageView]-|", options: [], metrics: nil, views: views)
+        
+        let titleLabelCenterY = NSLayoutConstraint(item: titleLabel, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1.0, constant: -4.0)
+        let countLabelCenterY = NSLayoutConstraint(item: countLabel, attribute: .top, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1.0, constant: 4.0)
+        let countLabelLeading = NSLayoutConstraint(item: countLabel, attribute: .leading, relatedBy: .equal, toItem: titleLabel, attribute: .leading, multiplier: 1.0, constant: 0.0)
+        
+        let posterImageViewRatio = NSLayoutConstraint(item: posterImageView, attribute: .width, relatedBy: .equal, toItem: posterImageView, attribute: .height, multiplier: 1.0, constant: 0.0)
+        
+        NSLayoutConstraint.activate(h)
+        NSLayoutConstraint.activate(posterImageViewV)
+        NSLayoutConstraint.activate([titleLabelCenterY, countLabelCenterY, countLabelLeading, posterImageViewRatio])
+    }
+}
+
+final class AlbumsViewController: UITableViewController {
+   
+    weak var delegate: PhotoPickerViewControllerDelegate?
+
+    var contentInset: UIEdgeInsets = .zero {
+        didSet {
+            tableView.contentInset.bottom = contentInset.bottom
+        }
+    }
+    
+    var assetsCollection: [Album]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(AlbumsViewController.cancel(_:)))
+        navigationItem.rightBarButtonItem = cancelButton
+        navigationItem.hidesBackButton = true
+        
+        tableView.hi.register(reusableCell: AlbumCell.self)
+        
+        tableView.tableFooterView = UIView()
+        
+        assetsCollection = fetchAlbumList()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = true
     }
+    
+    @objc fileprivate func cancel(_ sender: UIBarButtonItem) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    fileprivate func fetchAlbumIdentifier() -> String? {
+        let string = UserDefaults.standard.object(forKey: defaultAlbumIdentifier) as? String
+        return string
+    }
+    
+    fileprivate func fetchAlbum() -> Album? {
 
+        let identifier = fetchAlbumIdentifier()
+        
+        guard identifier != nil else {
+            return nil
+        }
+        
+        let options = PHFetchOptions()
+        options.predicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.image.rawValue)
+        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        
+        let result = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: nil)
+        if result.count <= 0 {
+            return nil
+        }
+        
+        guard let collection = result.firstObject else {
+            return nil
+        }
+        
+        let asset = PHAsset.fetchAssets(in: collection, options: options)
+        
+        return Album(asset: asset, count: asset.count, name: collection.localizedTitle, startDate: collection.startDate, identifier: collection.localIdentifier)
+    }
+    
+    fileprivate func fetchAlbumList() -> [Album] {
+        
+        let userAlbumsOptions = PHFetchOptions()
+        userAlbumsOptions.predicate = NSPredicate(format: "estimatedAssetCount > 0")
+        userAlbumsOptions.sortDescriptors = [NSSortDescriptor(key: "startDate", ascending: true)]
+        
+        var results: [PHFetchResult<PHAssetCollection>] = []
+        results.append(PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .albumRegular, options: nil))
+        results.append(PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: userAlbumsOptions))
+        
+        var list: [Album] = []
+        guard !results.isEmpty else {
+            return list
+        }
+        
+        let options = PHFetchOptions()
+        options.predicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.image.rawValue)
+        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        
+        results.forEach { result in
+            
+            result.enumerateObjects({ (collection: PHAssetCollection, idx: Int, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
+                let album = collection
+                guard  album.localizedTitle !=  NSLocalizedString("Recently Deleted", comment: "") else {
+                    return
+                }
+                
+                let asset = PHAsset.fetchAssets(in: album, options: options)
+                
+                let count: Int
+                switch album.assetCollectionType {
+                case .album:
+                    count = asset.count
+                case .smartAlbum:
+                    count = asset.count
+                case .moment:
+                    count = 0
+                }
+                
+                if count > 0 {
+                    list.append(Album(asset: asset, count: asset.count, name: collection.localizedTitle, startDate: collection.startDate, identifier: collection.localIdentifier))
+                }
+            })
+        }
+        
+        return list
+    }
+    
+    fileprivate func fetchImageWithAsset(_ asset: PHAsset?, targetSize: CGSize, imageResultHandler: @escaping (_ image: UIImage?)->Void) -> PHImageRequestID? {
+        guard let asset = asset else {
+            return nil
+        }
+        
+        let options = PHImageRequestOptions()
+        options.resizeMode = .exact
+        
+        let scale = UIScreen.main.scale
+        
+        let size = CGSize(width: targetSize.width * scale, height: targetSize.height * scale);
+        
+        return PHCachingImageManager.default().requestImage(for: asset,targetSize: size, contentMode: .aspectFill, options: options) { (result, info) in
+            imageResultHandler(result)
+        }
+    }
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return assetsCollection == nil ? 0 : assetsCollection!.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let cell: AlbumCell = tableView.hi.dequeueReusableCell(for: indexPath)
+        
+        if let album = assetsCollection?[indexPath.row] {
+            cell.countLabel.text = "(\(album.count))"
+            cell.titleLabel.text = album.name
+            
+            SafeDispatch.async(onQueue: DispatchQueue.global(qos: .default)) { [weak self] in
+                
+                _ = self?.fetchImageWithAsset(album.asset?.lastObject, targetSize: CGSize(width: 60, height: 60), imageResultHandler: { (image) in
+                    
+                    SafeDispatch.async {
+                        cell.posterImageView.image = image
+                    }
+                })
+            }
+        }
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        defer {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+        
+        guard let album = assetsCollection?[indexPath.row] else { return }
+        
+        let photoPicker = PhotoPickerViewController(collectionViewLayout: UICollectionViewFlowLayout())
+        photoPicker.imagesDidFetch = true
+        photoPicker.images = album.asset
+        photoPicker.contentInset = contentInset
+        photoPicker.delegate = delegate
+        photoPicker.title = album.name
+        
+        print("before push")
+        navigationController?.pushViewController(photoPicker, animated: true)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
--- Hi.story/Components/PhotoPicker/PhotoPickerViewController.swift
+++ Hi.story/Components/PhotoPicker/PhotoPickerViewController.swift
@@ -7,29 +7,303 @@
 //
 
 import UIKit
+import Hikit
+import Photos
 
-class PhotoPickerViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+final class PhotoCell: UICollectionViewCell, Reusable {
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+   
+    var imageManager: PHImageManager?
+    
+    var imageAsset: PHAsset? {
+        willSet {
+            guard let imageAsset = newValue else {
+                return
+            }
+            
+            let options = PHImageRequestOptions.sharedOptions
+            
+            let width: CGFloat = 188.0
+            let height = width
+            let targetSize = CGSize(width: width, height: height)
+            
+            DispatchQueue.global(qos: .default).async { [weak self] in
+                self?.imageManager?.requestImage(for: imageAsset, targetSize: targetSize, contentMode: .aspectFill, options: options) { [weak self] image, info in
+                    
+                    SafeDispatch.async { [weak self] in
+                        self?.imageView.image = image
+                    }
+                }
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        
+        contentView.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let views: [String: Any] = ["imageView": imageView]
+        
+        let h = NSLayoutConstraint.constraints(withVisualFormat: "H:|[imageView]|", options: [], metrics: nil, views: views)
+        let v = NSLayoutConstraint.constraints(withVisualFormat: "V:|[imageView]|", options: [], metrics: nil, views: views)
+        
+        NSLayoutConstraint.activate(h)
+        NSLayoutConstraint.activate(v)
+    }
 }
+
+protocol PhotoPickerViewControllerDelegate: class {
+    
+    func photoPickerControllerDidCancel(_ picker: PhotoPickerViewController)
+    
+    func photoPickerController(_ picker: PhotoPickerViewController, didFinishPickingPhoto photo: UIImage)
+}
+
+extension PhotoPickerViewControllerDelegate {
+    
+    func photoPickerControllerDidCancel(_ picker: PhotoPickerViewController) {
+    }
+    
+    func photoPickerController(_ picker: PhotoPickerViewController, didFinishPickingPhoto photo: UIImage) {
+    }
+}
+
+final class PhotoPickerViewController: UICollectionViewController, PHPhotoLibraryChangeObserver {
+    
+    var images: PHFetchResult<PHAsset>? {
+        didSet {
+            collectionView?.reloadData()
+        }
+    }
+    
+    weak var delegate: PhotoPickerViewControllerDelegate?
+    
+    var contentInset: UIEdgeInsets = .zero {
+        didSet {
+            collectionView?.contentInset.bottom = contentInset.bottom
+        }
+    }
+
+    var needsScrollToBottom: Bool = true
+    var imagesDidFetch: Bool = false
+    fileprivate let imageManager = PHCachingImageManager()
+    private var imageCacheController: ImageCacheController?
+    
+    private var navigationDidConfigure = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let collectionView = collectionView else { return }
+        
+        collectionView.contentInset.bottom = contentInset.bottom
+        
+        collectionView.backgroundColor = UIColor.white
+        collectionView.alwaysBounceVertical = true
+        automaticallyAdjustsScrollViewInsets = false
+        
+        collectionView.hi.register(reusableCell: PhotoCell.self)
+        
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            let gap: CGFloat = 1.0
+            
+            let width: CGFloat = (view.bounds.width - 5 * gap)  / 4
+            let height = width
+            layout.itemSize = CGSize(width: width, height: height)
+            
+            layout.minimumInteritemSpacing = gap
+            layout.minimumLineSpacing = gap
+            layout.sectionInset = UIEdgeInsets(top: gap + 64, left: gap, bottom: gap, right: gap)
+        }
+        
+        let backBarButtonItem = UIBarButtonItem(image: UIImage.hi.navBack, style: .plain, target: self, action: #selector(PhotoPickerViewController.back(_:)))
+        navigationItem.leftBarButtonItem = backBarButtonItem
+        
+        let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(PhotoPickerViewController.cancel(_:)))
+        navigationItem.rightBarButtonItem = cancelButton
+      
+        if !imagesDidFetch {
+            let options = PHFetchOptions()
+            options.sortDescriptors = [
+                NSSortDescriptor(key: "creationDate", ascending: true)
+            ]
+            title = "All Photos"
+            images = PHAsset.fetchAssets(with: .image, options: options)
+            
+        }
+        PHPhotoLibrary.shared().register(self)
+    }
+   
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if needsScrollToBottom {
+            scrollToBottom()
+            needsScrollToBottom = false
+        }
+        
+        if needsScrollToBottom {
+            scrollToBottom()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+      
+        if !navigationDidConfigure {
+            navigationDidConfigure = true
+            
+            guard var viewControllers = navigationController?.viewControllers else { return }
+            
+            let count = viewControllers.count
+            if !(viewControllers[count - 1] is AlbumsViewController) {
+                // previous is not albumsViewController
+                let albumsViewController = AlbumsViewController()
+                albumsViewController.contentInset = contentInset
+                albumsViewController.delegate = delegate
+                viewControllers.insert(albumsViewController, at: count - 1)
+                navigationController?.setViewControllers(viewControllers, animated: false)
+            }
+            
+            navigationController?.interactivePopGestureRecognizer?.delegate = self
+        }
+        
+        guard let images = images else { return }
+        
+        imageCacheController = ImageCacheController(imageManager: imageManager, images: images, preheatSize: 1)
+        
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+    }
+    
+    // MARK: Actions
+    
+    @objc private func back(_ sender: UIBarButtonItem) {
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    @objc private func cancel(_ sender: UIBarButtonItem) {
+        delegate?.photoPickerControllerDidCancel(self)
+    }
+    
+    private func scrollToBottom() {
+        guard let images = images, let collectionView = collectionView else { return }
+        
+        collectionView.scrollToItem(at: IndexPath(item: images.count - 1, section: 0), at: .centeredVertically, animated: false)
+    }
+    
+    private func pick(_ imageAsset: PHAsset) {
+        
+        let options = PHImageRequestOptions.sharedOptions
+        
+        imageManager.requestImageData(for: imageAsset, options: options, resultHandler: { [weak self] (data, String, imageOrientation, _) -> Void in
+            if let sSelf = self, let data = data, let image = UIImage(data: data) {
+                self?.delegate?.photoPickerController(sSelf, didFinishPickingPhoto: image)
+            }
+        })
+    }
+    
+    // MARK: UICollectionViewDataSource
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images?.count ?? 0
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell: PhotoCell = collectionView.hi.dequeueReusableCell(for: indexPath)
+        
+        cell.imageManager = imageManager
+        if let imageAsset = images?[indexPath.item] {
+            cell.imageAsset = imageAsset
+        }
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        if let imageAsset = images?[indexPath.item] {
+            pick(imageAsset)
+        }
+    }
+    
+    // MARK: - PHPhotoLibraryChangeObserver
+    
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        
+        guard let changeDetails = changeInstance.changeDetails(for: images!) else {
+            return
+        }
+        
+        self.images = changeDetails.fetchResultAfterChanges
+        
+        SafeDispatch.async {
+            // Loop through the visible cell indices
+            guard let
+                indexPaths = self.collectionView?.indexPathsForVisibleItems,
+                let changedIndexes = changeDetails.changedIndexes else {
+                    return
+            }
+            
+            for indexPath in indexPaths {
+                if changedIndexes.contains(indexPath.item) {
+                    let cell = self.collectionView?.cellForItem(at: indexPath) as! PhotoCell
+                    cell.imageAsset = changeDetails.fetchResultAfterChanges[indexPath.item]
+                }
+            }
+        }
+    }
+}
+
+extension PhotoPickerViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        if gestureRecognizer == navigationController?.interactivePopGestureRecognizer {
+            return true
+        }
+        return false
+    }
+}
+
+extension PHImageRequestOptions {
+    
+    static var sharedOptions: PHImageRequestOptions {
+        
+        let options = PHImageRequestOptions()
+        options.isSynchronous = true
+        options.version = .current
+        options.deliveryMode = .highQualityFormat
+        options.resizeMode = .exact
+        options.isNetworkAccessAllowed = true
+        
+        return options
+    }
+}
+
