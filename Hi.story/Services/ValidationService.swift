11d902afb80df92356e5459a3e15711c5f637a1c
xspyhack <xspyhack@gmail.com>
2016-12-31 22:16:57 +0800

    add validate username

--- Hi.story/Services/ValidationService.swift
+++ Hi.story/Services/ValidationService.swift
@@ -7,3 +7,102 @@
 //
 
 import Foundation
+import Hikit
+import RxSwift
+import RealmSwift
+
+enum ValidationResult {
+    case ok(message: String)
+    case empty
+    case validating
+    case failed(message: String)
+}
+
+extension ValidationResult {
+    var isValid: Bool {
+        switch self {
+        case .ok:
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+struct ValidationService {
+    
+    static func validate(username: String) -> Observable<ValidationResult> {
+        if username.isEmpty {
+            return .just(.empty)
+        }
+        
+        // this obviously won't be
+        if username.rangeOfCharacter(from: CharacterSet.alphanumerics.inverted) != nil {
+            return .just(.failed(message: "Username can only contain numbers or digits"))
+        }
+        
+        let loadingValue = ValidationResult.validating
+        
+        return RealmService.available(username: username)
+            .map { available in
+                if available {
+                    return .ok(message: "Username available")
+                } else {
+                    return .failed(message: "Username already taken")
+                }
+            }
+            .startWith(loadingValue)
+    }
+}
+
+
+/// Realm errors.
+public enum RealmError: Swift.Error {
+    /// Unknown error occurred.
+    case unknown
+    /// InitializeFailed error.
+    case initializeFailed(error: Swift.Error)
+}
+
+extension RealmError: CustomDebugStringConvertible {
+    /// A textual representation of `self`, suitable for debugging.
+    public var debugDescription: String {
+        switch self {
+        case .unknown:
+            return "Unknown error has occurred."
+        case let .initializeFailed(error):
+            return "Can't initialize realm: \(error)"
+        }
+    }
+}
+
+struct RealmService {
+   
+    static func available(username: String) -> Observable<Bool> {
+        
+        return Observable.create { (observer) -> Disposable in
+            
+            realmQueue.async {
+              
+                do {
+                    let realm = try Realm()
+                    
+                    if realm.objects(User.self).filter("username = %@", username).count > 0 {
+                        observer.onNext(false)
+                        observer.onCompleted()
+                    } else {
+                        observer.onNext(true)
+                        observer.onCompleted()
+                    }
+                    
+                } catch let error {
+                    return observer.on(.error(RealmError.initializeFailed(error: error)))
+                }
+                
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
+
