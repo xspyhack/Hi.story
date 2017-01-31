eec5818a2c38f9807ac2c16adc89efd121790864
xspyhack <xspyhack@gmail.com>
2017-01-31 15:49:03 +0800

    clean code

--- Hi.story/Components/PhotoPicker/AlbumsViewController.swift
+++ Hi.story/Components/PhotoPicker/AlbumsViewController.swift
@@ -10,16 +10,6 @@ import UIKit
 import Photos
 import Hikit
 
-struct Album {
-    var asset: PHFetchResult<PHAsset>?
-    var count = 0
-    var name: String?
-    var startDate: Date?
-    var identifier: String?
-}
-
-private let defaultAlbumIdentifier = "com.xspyhack.History.photoPicker"
-
 final class AlbumCell: UITableViewCell, Reusable {
     
     lazy var countLabel: UILabel = {
@@ -119,101 +109,7 @@ final class AlbumsViewController: UITableViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    fileprivate func fetchAlbumIdentifier() -> String? {
-        let string = UserDefaults.standard.object(forKey: defaultAlbumIdentifier) as? String
-        return string
-    }
     
-    fileprivate func fetchAlbum() -> Album? {
-
-        let identifier = fetchAlbumIdentifier()
-        
-        guard identifier != nil else {
-            return nil
-        }
-        
-        let options = PHFetchOptions()
-        options.predicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.image.rawValue)
-        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
-        
-        let result = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: nil)
-        if result.count <= 0 {
-            return nil
-        }
-        
-        guard let collection = result.firstObject else {
-            return nil
-        }
-        
-        let asset = PHAsset.fetchAssets(in: collection, options: options)
-        
-        return Album(asset: asset, count: asset.count, name: collection.localizedTitle, startDate: collection.startDate, identifier: collection.localIdentifier)
-    }
-    
-    fileprivate func fetchAlbumList() -> [Album] {
-        
-        let userAlbumsOptions = PHFetchOptions()
-        userAlbumsOptions.predicate = NSPredicate(format: "estimatedAssetCount > 0")
-        userAlbumsOptions.sortDescriptors = [NSSortDescriptor(key: "startDate", ascending: true)]
-        
-        var results: [PHFetchResult<PHAssetCollection>] = []
-        results.append(PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .albumRegular, options: nil))
-        results.append(PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: userAlbumsOptions))
-        
-        var list: [Album] = []
-        guard !results.isEmpty else {
-            return list
-        }
-        
-        let options = PHFetchOptions()
-        options.predicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.image.rawValue)
-        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
-        
-        results.forEach { result in
-            
-            result.enumerateObjects({ (collection: PHAssetCollection, idx: Int, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
-                let album = collection
-                guard  album.localizedTitle !=  NSLocalizedString("Recently Deleted", comment: "") else {
-                    return
-                }
-                
-                let asset = PHAsset.fetchAssets(in: album, options: options)
-                
-                let count: Int
-                switch album.assetCollectionType {
-                case .album:
-                    count = asset.count
-                case .smartAlbum:
-                    count = asset.count
-                case .moment:
-                    count = 0
-                }
-                
-                if count > 0 {
-                    list.append(Album(asset: asset, count: asset.count, name: collection.localizedTitle, startDate: collection.startDate, identifier: collection.localIdentifier))
-                }
-            })
-        }
-        
-        return list
-    }
-    
-    fileprivate func fetchImageWithAsset(_ asset: PHAsset?, targetSize: CGSize, imageResultHandler: @escaping (_ image: UIImage?)->Void) -> PHImageRequestID? {
-        guard let asset = asset else {
-            return nil
-        }
-        
-        let options = PHImageRequestOptions()
-        options.resizeMode = .exact
-        
-        let scale = UIScreen.main.scale
-        
-        let size = CGSize(width: targetSize.width * scale, height: targetSize.height * scale);
-        
-        return PHCachingImageManager.default().requestImage(for: asset,targetSize: size, contentMode: .aspectFill, options: options) { (result, info) in
-            imageResultHandler(result)
-        }
-    }
     
     // MARK: - Table view data source
     
@@ -234,9 +130,9 @@ final class AlbumsViewController: UITableViewController {
             cell.countLabel.text = "(\(album.count))"
             cell.titleLabel.text = album.name
             
-            SafeDispatch.async(onQueue: DispatchQueue.global(qos: .default)) { [weak self] in
+            SafeDispatch.async(onQueue: DispatchQueue.global(qos: .default)) {
                 
-                _ = self?.fetchImageWithAsset(album.asset?.lastObject, targetSize: CGSize(width: 60, height: 60), imageResultHandler: { (image) in
+                _ = fetchImageWithAsset(album.asset?.lastObject, targetSize: CGSize(width: 60, height: 60), imageResultHandler: { (image) in
                     
                     SafeDispatch.async {
                         cell.posterImageView.image = image
