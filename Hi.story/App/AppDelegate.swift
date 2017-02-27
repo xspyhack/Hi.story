9a8a0eda6721ccb75d7812e1a5e52f57ad1f5dca
xspyhack <xspyhack@gmail.com>
2017-02-27 20:26:15 +0800

    setup spotlight

--- Hi.story/App/AppDelegate.swift
+++ Hi.story/App/AppDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 import Hikit
 import RealmSwift
 import WatchConnectivity
+import CoreSpotlight
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -79,6 +80,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
         
+        if Defaults.spotlightEnabled {
+            
+            CSSearchableIndex.default().deleteAllSearchableItems { error in
+                guard error == nil else {
+                    return
+                }
+                
+                indexFeedSearchableItems()
+            }
+        } else {
+            CSSearchableIndex.default().deleteAllSearchableItems(completionHandler: nil)
+        }
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
