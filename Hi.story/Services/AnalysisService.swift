e2b6c6b7b4cb6fb00f30cdad021123fe1ef26308
xspyhack <xspyhack@gmail.com>
2017-03-04 14:22:15 +0800

    add photo cache service

---
+++ Hi.story/Services/AnalysisService.swift
@@ -0,0 +1,9 @@
+//
+//  AnalysisService.swift
+//  Hi.story
+//
+//  Created by bl4ckra1sond3tre on 01/03/2017.
+//  Copyright © 2017 bl4ckra1sond3tre. All rights reserved.
+//
+
+import Foundation
--- Hi.story/Services/CacheService.swift
+++ Hi.story/Services/CacheService.swift
@@ -9,8 +9,33 @@
 import UIKit
 import Kingfisher
 
-class Cache {
-    static let shared = Cache()
+class PhotoCache {
+    static let shared = PhotoCache()
+    
+    private let cache = ImageCache(name: "photos")
+    
+    private init() {
+        cache.pathExtension = "jpg"
+        cache.maxCachePeriodInSecond = 60 * 5 // never expiring
+    }
+    
+    func store(_ image: UIImage, forKey key: String, completionHandler: (() -> Void)? = nil) {
+        cache.store(image, forKey: key, toDisk: true, completionHandler: completionHandler)
+    }
+    
+    func removeIfExisting(forKey key: String) {
+        cache.removeImage(forKey: key, fromDisk: true, completionHandler: nil)
+    }
+    
+    func retrieveImageInDiskCache(forKey key: String, scale: CGFloat = 1.0) -> UIImage? {
+        return cache.retrieveImageInDiskCache(forKey: key, options: [.scaleFactor(scale)])
+    }
+    
+    func filePath(forKey key: String) -> String {
+        return cache.cachePath(forComputedKey: key)
+    }
+    
+    static let sharedCache = PhotoCache.shared.cache
 }
 
 private let cacheName = "Images"
@@ -20,7 +45,7 @@ class CacheService {
     
     static let shared = CacheService()
     
-    fileprivate let cache = ImageCache(name: cacheName, path: String.hi.documentsPath)
+    private let cache = ImageCache(name: cacheName, path: String.hi.documentsPath)
     
     private init() {
         cache.pathExtension = "png"
