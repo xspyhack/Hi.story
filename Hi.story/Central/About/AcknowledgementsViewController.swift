737d47c2743d7e1e157c3e991991b7e5e54dccea
xspyhack <xspyhack@gmail.com>
2017-03-18 21:13:17 +0800

    add acknowledgements

--- Hi.story/Central/About/AcknowledgementsViewController.swift
+++ Hi.story/Central/About/AcknowledgementsViewController.swift
@@ -8,88 +8,112 @@
 
 import UIKit
 
+struct Project {
+    let name: String
+    let license: String
+    let urlString: String
+}
+
 class AcknowledgementsViewController: UITableViewController {
+    
+    enum Section: Int {
+        case licenses
+        case covers
+        
+        var annotation: String {
+            switch self {
+            case .licenses: return "Licenses"
+            case .covers: return "Cover Credits"
+            }
+        }
+        
+        static var count: Int {
+            return Section.covers.rawValue + 1
+        }
+    }
+    
+    private(set) lazy var projects: [Project] = {
+        return [
+            Project(name: "RxSwift", license: "ReactiveX - Licensed under MIT", urlString: "https://github.com/ReactiveX/RxSwift"),
+            Project(name: "RxDataSources", license: "RxSwiftCommunity - Licensed under MIT", urlString: "https://github.com/RxSwiftCommunity/RxDataSources"),
+            Project(name: "Kingfisher", license: "onvcat - Licensed under MIT", urlString: "https://github.com/onevcat/Kingfisher"),
+            Project(name: "Realm", license: "realm - Apache License", urlString: "https://github.com/realm/realm-cocoa"),
+            Project(name: "KeyboardMan", license: "nixzhu - Licensed under MIT", urlString: "https://github.com/nixzhu/KeyboardMan"),
+            Project(name: "Proposer", license: "nixzhu - Licensed under MIT", urlString: "https://github.com/nixzhu/Proposer/"),
+            Project(name: "Coder", license: "@xspyhack - Coded Hi.story", urlString: "https://github.com/xspyhack"),
+            Project(name: "Designer", license: "@xspyhack - Designed Hi.story", urlString: "https://github.com/xspyhack"),
+        ]
+    }()
+    
+    private var covers: Project = Project(name: "Illustration", license: "@Andrey Prokopenko", urlString: "https://dribbble.com/Pro_Art/projects/385064-Illustration")
+    
+    struct Constant {
+        static let rowHeight: CGFloat = 60.0
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        tableView.rowHeight = Constant.rowHeight
+        tableView.backgroundColor = UIColor.hi.background
+        tableView.tableFooterView = UIView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func safari(project: Project) {
+        hi.open(URL(string: project.urlString)!)
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        switch Section(rawValue: section) {
+        case .licenses?: return projects.count
+        case .covers?: return 1
+        default: return 0
+        }
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        switch Section(rawValue: indexPath.section) {
+        case .licenses?:
+            let project = projects.safe[indexPath.row]
+            cell.textLabel?.text = project?.name
+            cell.detailTextLabel?.text = project?.license
+        case .covers?:
+            cell.textLabel?.text = covers.name
+            cell.detailTextLabel?.text = covers.license
+        default:
+            break
+        }
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let section = Section(rawValue: section) else { fatalError() }
+        
+        return section.annotation
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        defer {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+        
+        switch Section(rawValue: indexPath.section) {
+        case .licenses?:
+            guard let project = projects.safe[indexPath.row] else { return }
+            safari(project: project)
+        case .covers?:
+            safari(project: covers)
+        default:
+            break
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
