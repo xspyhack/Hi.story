b61d1cf1fee2c821a6e5eb9ecb282482d0060aef
xspyhack <xspyhack@gmail.com>
2018-05-06 22:03:49 +0800

    fix cache

--- Hi.cache/Caches.swift
+++ Hi.cache/Caches.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public struct Caches<Object>: Caching {
     
-    var caches: [AnyCache<Object>]
+    var caches: [AnyAsyncCache<Object>]
     
     public var serializer: AnyCacheSerializer<Object>
     
@@ -23,12 +23,36 @@ public struct Caches<Object>: Caching {
     }
     
     public func retrieve(forKey key: String) -> Object? {
+        for cache in caches {
+            if let object = cache.retrieve(forKey: key) {
+                return object
+            }
+        }
         return nil
+    }
+
+    public func retrieve(forKey key: String, completion: @escaping (Object?, String) -> Void) {
+        let semaphore = DispatchSemaphore(value: 0)
+
+        for cache in caches {
+            cache.retrieve(forKey: key) { object, key in
+                if let object = object {
+                    completion(object, key)
+                    semaphore.signal()
+                    return
+                }
+                semaphore.signal()
+            }
+
+            let _ = semaphore.wait(timeout: .distantFuture)
+        }
+
+        completion(nil, key)
     }
     
     public func remove(forKey key: String) {
         coordinate({ cache, finished in
-            cache.remove(forKey: key)
+            cache.remove(forKey: key, completionHandler: finished)
         }, completion: {
             
         })
@@ -46,22 +70,21 @@ public struct Caches<Object>: Caching {
         return false
     }
     
-    private func coordinate(_ each: ((AnyCache<Object>, (() -> Void)?) -> Void), completion: (() -> Void)?) {
-        // Count starts with the count of caches
-        let count = Int32(caches.count)
-        
-        let finish: () -> Void = {
-            // Safely decrement the count
-            
-            // If the count is 0, we're received all of the callbacks.
-            if count == 0 {
-                // Call the completion
-                completion?()
+    private func coordinate(_ each: ((AnyAsyncCache<Object>, (@escaping (String) -> Void)) -> Void), completion: (() -> Void)?) {
+
+        let group = DispatchGroup()
+
+        // Kick off the work for each cache
+        caches.forEach {
+            group.enter()
+            each($0) { key in
+                group.leave()
             }
         }
-        
-        // Kick off the work for each cache
-        caches.forEach { each($0, finish) }
+
+        if let completion = completion {
+            group.notify(queue: DispatchQueue.main, execute: completion)
+        }
     }
     
 }
